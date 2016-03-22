3166ad3b9782a430abeb6eeba83083e9e17ed8ed
alexvahrushev92 <gnpr11@gmail.com>
2016-03-22 22:27:23 +0000

    Merge branches 'alex92' and 'master' of https://github.com/Taity-mini/CASA_CM2003 into alex92
    # Conflicts:
    #	index.html

--- beer/js/index.js
+++ beer/js/index.js
@@ -10,12 +10,12 @@ $(document).ready(function() {
   $('#liquid') // I Said Fill 'Er Up!
     .delay(3400)
     .animate({
-      height: '170px'
+      height: '120px'
     }, 2500);
   
   $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
     .delay(3400)
     .animate({
-      bottom: '200px'
+      bottom: '138px'
       }, 2500);
   });
