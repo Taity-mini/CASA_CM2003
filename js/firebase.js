3f38fbc8e03d4f53bd99cb62964a88d6d96d2122
Craig Robertson <1109847@rgu.ac.uk>
2016-04-23 13:55:09 +0100

    fixed firebase script now pushes to data base

--- js/firebase.js
+++ js/firebase.js
@@ -2,21 +2,22 @@
  * Created by Craig Robertson on 19/04/2016.
  */
 
-var casaDataRef = new Firebase('https://pub-crawl.firebaseio.com/');
-var crawlName;
-var crawlLocation;
+var casaDataRef = new Firebase('https://casa-pubcrawl.firebaseio.com/routes');
+var crawlName = "";
+var crawlLocation = "";
 var crawlURL = "";
 var crawlRating = '*';
 var newKey;
 
-console.log("Name "+crawlName + "location" + crawlLocation +"crawlURL");
+console.log("Name: "+ crawlName + "location: " + crawlLocation +"crawlURL");
 
 $(document).ready(function(){
     $('#start-route').on("click", function()
     {
         push();
         //$(location).attr('href', './display/?id=' + newKey)
-        window.location.replace ('../display/?id=' + newKey);
+         window.location.replace ('../display/?id=' + newKey);
+        //console.log("Name: "+ crawlName + "location: " + crawlLocation +"crawlURL: " + crawlURL + "Key: " + newKey);
     });
 });
 
@@ -25,7 +26,7 @@ function push(){
     crawlName = $('.crawlname').val();
     crawlLocation = $('#location-search').val();
 
-    var newPush = casaDataRef.child('routes').push({crawlName: crawlName, crawlLocation: crawlLocation, crawlURL: crawlURL});
+    var newPush = casaDataRef.push({ crawlName: crawlName, crawlLocation: crawlLocation, crawlURL: 'url' });
     newKey = newPush.key();
     console.log(newPush);
     console.log(newKey);
